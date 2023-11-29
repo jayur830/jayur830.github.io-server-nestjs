6243881a3155202ca5ae76a6cbb795c1d753c28e
enlightenL <jaeyeol.lee@enlighten.kr>
2023-11-30 00:27:04 +0900

    ci: cloud build test

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,11 @@
-// import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // const configService = app.get(ConfigService);
-  // const port = configService.get('PORT');
-  const port = 8080;
-  // const env = configService.get('NODE_ENV');
-  const env = 'development';
+  const port = process.env.PORT;
+  const env = process.env.NODE_ENV;
   await app.listen(port);
   console.log('Environment:', env);
   console.log(`Server is listening port ${port}.`);
--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,16 +1,16 @@
 // import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-// import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 // import { APP_INTERCEPTOR } from '@nestjs/core';
 // import { GraphQLModule } from '@nestjs/graphql';
-// import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-// import { AboutMe } from '@/entities/about_me.entity';
-// import { CompanyLogo } from '@/entities/company_logo.entity';
-// import { ResumeHistory } from '@/entities/resume_history.entity';
-// import { ResumeHistoryDetail } from '@/entities/resume_history_detail.entity';
-// import { ResumeInfo } from '@/entities/resume_info.entity';
-// import { User } from '@/entities/user.entity';
+import { AboutMe } from '@/entities/about_me.entity';
+import { CompanyLogo } from '@/entities/company_logo.entity';
+import { ResumeHistory } from '@/entities/resume_history.entity';
+import { ResumeHistoryDetail } from '@/entities/resume_history_detail.entity';
+import { ResumeInfo } from '@/entities/resume_info.entity';
+import { User } from '@/entities/user.entity';
 // import { TransactionalInterceptor } from '@/interceptors/transactional/transactional.interceptor';
 // import { AuthModule } from '@/modules/auth/auth.module';
 // import { ResumeModule } from '@/modules/resume/resume.module';
@@ -23,27 +23,37 @@ import { YearScalar } from '@/scalars/date/year.scalar';
 
 @Module({
   imports: [
-    // ConfigModule.forRoot({
-    //   isGlobal: true,
-    // }),
-    // TypeOrmModule.forRootAsync({
-    //   imports: [ConfigModule],
-    //   inject: [ConfigService],
-    //   async useFactory(configService: ConfigService) {
-    //     console.log(configService.get<string>('MYSQL_HOST'));
-    //     return {
-    //       type: 'mysql',
-    //       host: configService.get<string>('MYSQL_HOST'),
-    //       port: configService.get<number>('MYSQL_PORT'),
-    //       username: configService.get<string>('MYSQL_USERNAME'),
-    //       password: configService.get<string>('MYSQL_PASSWORD'),
-    //       database: configService.get<string>('MYSQL_DATABASE'),
-    //       entities: [ResumeInfo, ResumeHistory, ResumeHistoryDetail, CompanyLogo, User, AboutMe],
-    //       synchronize: false,
-    //       logging: true,
-    //     };
-    //   },
-    // }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      async useFactory() {
+        // return {
+        //   type: 'mysql',
+        //   host: configService.get<string>('MYSQL_HOST'),
+        //   port: configService.get<number>('MYSQL_PORT'),
+        //   username: configService.get<string>('MYSQL_USERNAME'),
+        //   password: configService.get<string>('MYSQL_PASSWORD'),
+        //   database: configService.get<string>('MYSQL_DATABASE'),
+        //   entities: [ResumeInfo, ResumeHistory, ResumeHistoryDetail, CompanyLogo, User, AboutMe],
+        //   synchronize: false,
+        //   logging: true,
+        // };
+        return {
+          type: 'mysql',
+          host: process.env.MYSQL_HOST,
+          port: +process.env.MYSQL_PORT,
+          username: process.env.MYSQL_USERNAME,
+          password: process.env.MYSQL_PASSWORD,
+          database: process.env.MYSQL_DATABASE,
+          entities: [ResumeInfo, ResumeHistory, ResumeHistoryDetail, CompanyLogo, User, AboutMe],
+          synchronize: false,
+          logging: true,
+        };
+      },
+    }),
     // GraphQLModule.forRoot<ApolloDriverConfig>({
     //   path: '/api/graphql',
     //   autoSchemaFile: 'schema.gql',

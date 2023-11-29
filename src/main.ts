9487724942a613460db5a48ff2f307fceafda212
enlightenL <jaeyeol.lee@enlighten.kr>
2023-11-29 23:57:11 +0900

    ci: cloud build test

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
-import { ConfigService } from '@nestjs/config';
+// import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
+  // const configService = app.get(ConfigService);
   // const port = configService.get('PORT');
   const port = 8080;
-  const env = configService.get('NODE_ENV');
+  // const env = configService.get('NODE_ENV');
+  const env = 'development';
   await app.listen(port);
   console.log('Environment:', env);
   console.log(`Server is listening port ${port}.`);

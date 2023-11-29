4a7c6aa429f35f5283dccd628ef8d2e98dcf10a2
enlightenL <jaeyeol.lee@enlighten.kr>
2023-11-29 22:10:47 +0900

    fix: cors 설정 삭제

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
   const env = configService.get('NODE_ENV');

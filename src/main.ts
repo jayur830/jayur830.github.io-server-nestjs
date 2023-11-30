d4aa442c787c22b39af94980e87d0528c954fc88
enlightenL <jaeyeol.lee@enlighten.kr>
2023-11-30 23:48:08 +0900

    ci: 코드 주석해제

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
-  console.log('env:', process.env);
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT;
   const env = process.env.NODE_ENV;
--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,9 +1,9 @@
-// import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 // import { APP_INTERCEPTOR } from '@nestjs/core';
-// import { GraphQLModule } from '@nestjs/graphql';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AboutMe } from '@/entities/about_me.entity';
 import { CompanyLogo } from '@/entities/company_logo.entity';
@@ -12,60 +12,41 @@ import { ResumeHistoryDetail } from '@/entities/resume_history_detail.entity';
 import { ResumeInfo } from '@/entities/resume_info.entity';
 import { User } from '@/entities/user.entity';
 // import { TransactionalInterceptor } from '@/interceptors/transactional/transactional.interceptor';
-// import { AuthModule } from '@/modules/auth/auth.module';
-// import { ResumeModule } from '@/modules/resume/resume.module';
+import { AuthModule } from '@/modules/auth/auth.module';
+import { ResumeModule } from '@/modules/resume/resume.module';
 import { DateScalar } from '@/scalars/date/date.scalar';
 import { MonthScalar } from '@/scalars/date/month.scalar';
 import { YearScalar } from '@/scalars/date/year.scalar';
 
-// import { InfoModule } from '../info/info.module';
-// import { TechModule } from '../tech/tech.module';
+import { InfoModule } from '../info/info.module';
+import { TechModule } from '../tech/tech.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      async useFactory() {
-        // return {
-        //   type: 'mysql',
-        //   host: configService.get<string>('MYSQL_HOST'),
-        //   port: configService.get<number>('MYSQL_PORT'),
-        //   username: configService.get<string>('MYSQL_USERNAME'),
-        //   password: configService.get<string>('MYSQL_PASSWORD'),
-        //   database: configService.get<string>('MYSQL_DATABASE'),
-        //   entities: [ResumeInfo, ResumeHistory, ResumeHistoryDetail, CompanyLogo, User, AboutMe],
-        //   synchronize: false,
-        //   logging: true,
-        // };
-        const config: TypeOrmModuleOptions = {
-          type: 'mysql',
-          host: process.env.MYSQL_HOST,
-          port: +process.env.MYSQL_PORT,
-          username: process.env.MYSQL_USERNAME,
-          password: process.env.MYSQL_PASSWORD,
-          database: process.env.MYSQL_DATABASE,
-          entities: [ResumeInfo, ResumeHistory, ResumeHistoryDetail, CompanyLogo, User, AboutMe],
-          synchronize: false,
-          logging: true,
-        };
-        console.log('config:', config);
-        return config;
-      },
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: +process.env.MYSQL_PORT,
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      entities: [ResumeInfo, ResumeHistory, ResumeHistoryDetail, CompanyLogo, User, AboutMe],
+      synchronize: false,
+      logging: true,
     }),
-    // GraphQLModule.forRoot<ApolloDriverConfig>({
-    //   path: '/api/graphql',
-    //   autoSchemaFile: 'schema.gql',
-    //   driver: ApolloDriver,
-    //   playground: true,
-    // }),
-    // AuthModule,
-    // ResumeModule,
-    // TechModule,
-    // InfoModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      path: '/api/graphql',
+      autoSchemaFile: 'schema.gql',
+      driver: ApolloDriver,
+      playground: true,
+    }),
+    AuthModule,
+    ResumeModule,
+    TechModule,
+    InfoModule,
   ],
   providers: [
     // {
